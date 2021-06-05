5cec6a74860a9247aba4318e34e5c7be8f593794
AviKav <avikav@avikav.net>
2021-06-05 15:34:22 -0400

    Attempt hash-based routing

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-
+import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,7 +8,11 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		//target: '#svelte'
+		ssr: false,
+		adapter: adapter({
+			fallback: '200.html'
+		})
 	}
 };
 
