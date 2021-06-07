bb68b545ff008a752f44926f92f9fe8bb2e0dfe0
AviKav <avikav@avikav.net>
2021-06-07 18:01:39 -0400

    Redo dist and fix injection

---
+++ dist.fish
@@ -0,0 +1,43 @@
+#!/usr/bin/env fish
+. ./dist-env
+mkdir -p intermediate/worktree
+
+set short_hash (git rev-parse --short HEAD)
+
+set asset_path intermediate/worktree/content-addressed-assets
+
+git worktree list --porcelain \
+| grep -A 2 'worktree 'zi(pwd)/{$asset_path} \
+| grep 'branch refs/heads/'(basename $asset_path) >/dev/null;
+and git worktree repair $asset_path
+or begin
+    git worktree remove -f $asset_path 2>/dev/null
+    git worktree add $asset_path
+end
+
+function prop
+    echo \"$argv[1]:\" \"$argv[2]\",
+end
+
+set generated (begin \
+    echo '// @version '$short_hash
+    echo '// ==/UserScript=='
+    echo
+    echo 'let data = {'
+    for file in (find ./public -type f -not -name 'index.html' -print0 | string split0)
+        set hash (sha256sum $file | awk '{ print $1 }' | tr -d \n )
+        mkdir -p {$asset_path}/assets/{$hash}/
+        rsync $file {$asset_path}/assets/{$hash}/
+
+        echo \"(echo -n $file | sed 's!./public!!')'": {'
+        begin
+            prop sri sha256-{$hash}
+            prop web {$host}/assets/{$hash}/(basename $file)
+        end
+        echo '},'
+    end
+    echo '}'
+end | string collect)
+echo -n $generated | sed "/__INSERT__/ r /dev/stdin" userscript.template.js > intermediate/userscript.js
+# Remove marker
+sed -i '/__INSERT__/{N;s/__INSERT__\n//;}' intermediate/userscript.js
--- userscript.template.js
+++ userscript.template.js
@@ -1,3 +1,4 @@
+// ==UserScript==
 // @name MD Svelte Shim
 // @namespace https://github.com/AviKav/MD-Porcelain
 // @match https://*.mangadex.org/*
@@ -6,4 +7,31 @@
 // @noframes
 __INSERT__
 
-window.document.getElementsByTagName('html')[0].innerHTML = str;
+function populate(key) {
+    return `href='${data[key].web}' integrity='${data[key].sri}'`
+}
+
+let str = `<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width,initial-scale=1'>
+
+	<title>MD Svelte Shim</title>
+
+	<link rel='icon' type='image/png' ${populate("/favicon.png")}>
+	<link rel='stylesheet' ${populate("/global.css")}>
+	<link rel='stylesheet' ${populate("/build/bundle.css")}>
+</head>
+
+<body>
+</body>
+</html>
+`
+
+unsafeWindow.document.getElementsByTagName('html')[0].innerHTML = str
+
+script = unsafeWindow.document.createElement('script');
+script.src = data["/build/bundle.js"].web;
+script.integrity = data["/build/bundle.js"].sri;
+unsafeWindow.document.getElementsByTagName('head')[0].appendChild(script);
